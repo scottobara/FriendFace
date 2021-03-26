79d19109b7efba6114336d70bca24bdbd1ce1020
Scott Obara <scott@rubbscreations.com>
2021-03-26 13:39:54 +1100

    Unsaved changes

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -12,24 +12,24 @@ import CoreData
 //    var results: [Result]
 //}
 
-//struct User: Codable {
-//    var id: UUID
-//    var isActive: Bool
-//    var name: String
-//    var age: Int
-//    var company: String
-//    var email: String
-//    var address: String
-//    var about: String
-//    var registered: Date
-//    var tags: [String]
-//    var friends: [Friend]
-//}
-//
-//struct Friend: Codable {
-//    var id: UUID
-//    var name: String
-//}
+struct UserStruct: Codable {
+    var id: UUID
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date
+    var tags: [String]
+    var friends: [FriendStruct]
+}
+
+struct FriendStruct: Codable {
+    var id: UUID
+    var name: String
+}
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -39,71 +39,83 @@ struct ContentView: View {
         animation: .default)
     private var users: FetchedResults<User>
     
-    //@State private var users = [User]()
+//    @State private var dataLoaded = UserDefaults.standard.bool(forKey: "Loaded")
+    @State private var usersStruct = [UserStruct]()
+    @AppStorage("dataLoaded") var dataLoaded = false
     
     var body: some View {
         
         NavigationView {
             
-//            List(users, id: \.id) { user in
-//                NavigationLink(destination: UserDetailView(user: user)) {
-//                    VStack(alignment: .leading) {
-//                        Text(user.name ?? "UNKNOWN")
-//                            .font(.headline)
-//                        Text(user.email ?? "UNKNOWN")
-//                    }
-//                }
-//            }
-            List {
-                ForEach(users, id: \.id) { user in
-                    Section(header: Text(user.wrappedName)) {
-                        ForEach(user.friendsArray, id: \.id) { friend in
-                            Text(friend.wrappedName)
-                        }
+            List(usersStruct, id: \.id) { user in
+                NavigationLink(destination: UserDetailView(user: user)) {
+                    VStack(alignment: .leading) {
+                        Text(user.name ?? "UNKNOWN")
+                            .font(.headline)
+                        Text(user.email ?? "UNKNOWN")
                     }
                 }
             }
-//            .onAppear(perform: loadData)
+//            List {
+//                ForEach(users, id: \.id) { user in
+//                    Section(header: Text(user.wrappedName)) {
+//                        ForEach(user.friendsArray, id: \.id) { friend in
+//                            Text(friend.wrappedName)
+//                        }
+//                    }
+//                }
+//            }
+            .onAppear(perform: loadData)
             .navigationTitle("FriendFace")
         }
     }
     
-//    func loadData() {
-//
-//        //    1. Creating the URL we want to read.
-//        //    2. Wrapping that in a URLRequest, which allows us to configure how the URL should be accessed.
-//        //    3. Create and start a networking task from that URL request.
-//        //    4. Handle the result of that networking task.
-//
-//        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
-//            print("Invalid URL")
-//            return
-//        }
-//
-//        let request = URLRequest(url: url)
-//
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//
-//            if let data = data {
-//                //https://www.hackingwithswift.com/example-code/language/how-to-use-iso-8601-dates-with-jsondecoder-and-codable
-//                let decoder = JSONDecoder()
-//                decoder.dateDecodingStrategy = .iso8601
-//
-//                if let decodedResponse = try? decoder.decode([User].self, from: data) {
-//                    // we have good data – go back to the main thread
-//                    DispatchQueue.main.async {
-//                        // update our UI
-//                        self.users = decodedResponse
-//                    }
-//                    // everything is good, so we can exit
-//                    return
-//                }
-//            }
-//            // if we're still here it means there was a problem
-//            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-//
-//        }.resume()
-//    }
+    func loadData() {
+        dataLoaded = false
+        print("\(dataLoaded)")
+        
+        if (dataLoaded == true) {
+            return
+        } else {
+            //    1. Creating the URL we want to read.
+            //    2. Wrapping that in a URLRequest, which allows us to configure how the URL should be accessed.
+            //    3. Create and start a networking task from that URL request.
+            //    4. Handle the result of that networking task.
+            
+            guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+                print("Invalid URL")
+                return
+            }
+            
+            let request = URLRequest(url: url)
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                
+                if let data = data {
+                    //https://www.hackingwithswift.com/example-code/language/how-to-use-iso-8601-dates-with-jsondecoder-and-codable
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    
+                    if let decodedResponse = try? decoder.decode([UserStruct].self, from: data) {
+                        // we have good data – go back to the main thread
+                        DispatchQueue.main.async {
+                            // update our UI
+                            self.usersStruct = decodedResponse
+                        }
+                        // everything is good, so we can exit
+                        return
+                    }
+                }
+                // if we're still here it means there was a problem
+                print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+                
+            }.resume()
+            
+        }
+        
+        dataLoaded = true
+        print("\(UserDefaults.standard.bool(forKey: "Loaded"))")
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- FriendFace/UserDetailView.swift
+++ FriendFace/UserDetailView.swift
@@ -5,35 +5,35 @@
 //  Created by Scott Obara on 14/2/21.
 //
 
-//import SwiftUI
-//
-//struct UserDetailView: View {
-//    
-//    let user: User
-//    
-//    var body: some View {
-//        ScrollView(.vertical) {
-//            VStack(alignment: .leading, spacing: 10.0) {
-//                Text(user.name)
-//                    .font(.headline)
-//                Text(user.about)
-//                    //.layoutPriority(1)
-//                Text("Friends")
-//                    .font(.headline)
-//                    .padding(.top)
-//                ForEach(user.friends, id: \.id) { friend in
-//                    Text(friend.name)
-//                }
-//                Spacer()
-//            }
-//            .padding()
-//        }
-//    }
-//}
-//
-//struct UserDetailView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        UserDetailView(user: User(id: UUID(), isActive: false, name: "Alford Rodriguez", age: 21, company: "Imkan", email: "alfordrodriguez@imkan.com", address: "907 Nelson Street, Cotopaxi, South Dakota, 5913", about: "Occaecat consequat elit aliquip magna laboris dolore laboris sunt officia adipisicing reprehenderit sunt. Do in proident consectetur labore. Laboris pariatur quis incididunt nostrud labore ad cillum veniam ipsum ullamco. Dolore laborum commodo veniam nisi. Eu ullamco cillum ex nostrud fugiat eu consequat enim cupidatat. Non incididunt fugiat cupidatat reprehenderit nostrud eiusmod eu sit minim do amet qui cupidatat. Elit aliquip nisi ea veniam proident dolore exercitation irure est deserunt.", registered: Date(), tags: ["tag1", "tag2", "tag3"], friends: [Friend(id: UUID(), name: "Hawkins Patel"), Friend(id: UUID(), name: "Jewel Sexton"), Friend(id: UUID(), name: "Berger Robertson"), Friend(id: UUID(), name: "Hess Ford"), Friend(id: UUID(), name: "Bonita White"), Friend(id: UUID(), name: "Sheryl Robinson"), Friend(id: UUID(), name: "Karin Collins"), Friend(id: UUID(), name: "Pace English"), Friend(id: UUID(), name: "Pauline Dawson"), Friend(id: UUID(), name: "Russo Carlson"), Friend(id: UUID(), name: "Josefina Rivas")]))
-//    }
-//}
+import SwiftUI
+
+struct UserDetailView: View {
+    
+    let user: UserStruct
+    
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack(alignment: .leading, spacing: 10.0) {
+                Text(user.name)
+                    .font(.headline)
+                Text(user.about)
+                    //.layoutPriority(1)
+                Text("Friends")
+                    .font(.headline)
+                    .padding(.top)
+                ForEach(user.friends, id: \.id) { friend in
+                    Text(friend.name)
+                }
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+struct UserDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserDetailView(user: UserStruct(id: UUID(), isActive: false, name: "Alford Rodriguez", age: 21, company: "Imkan", email: "alfordrodriguez@imkan.com", address: "907 Nelson Street, Cotopaxi, South Dakota, 5913", about: "Occaecat consequat elit aliquip magna laboris dolore laboris sunt officia adipisicing reprehenderit sunt. Do in proident consectetur labore. Laboris pariatur quis incididunt nostrud labore ad cillum veniam ipsum ullamco. Dolore laborum commodo veniam nisi. Eu ullamco cillum ex nostrud fugiat eu consequat enim cupidatat. Non incididunt fugiat cupidatat reprehenderit nostrud eiusmod eu sit minim do amet qui cupidatat. Elit aliquip nisi ea veniam proident dolore exercitation irure est deserunt.", registered: Date(), tags: ["tag1", "tag2", "tag3"], friends: [FriendStruct(id: UUID(), name: "Hawkins Patel"), FriendStruct(id: UUID(), name: "Jewel Sexton"), FriendStruct(id: UUID(), name: "Berger Robertson"), FriendStruct(id: UUID(), name: "Hess Ford"), FriendStruct(id: UUID(), name: "Bonita White"), FriendStruct(id: UUID(), name: "Sheryl Robinson"), FriendStruct(id: UUID(), name: "Karin Collins"), FriendStruct(id: UUID(), name: "Pace English"), FriendStruct(id: UUID(), name: "Pauline Dawson"), FriendStruct(id: UUID(), name: "Russo Carlson"), FriendStruct(id: UUID(), name: "Josefina Rivas")]))
+    }
+}
 
